22bff7a65a03e1ffc766f575e9a9d23fa2a84427
Al <alastairpatrick@gmail.com>
2022-09-14 09:34:22 -0700

    qos_from_absolute_time()

--- src/qos/time.h
+++ src/qos/time.h
@@ -3,11 +3,17 @@
 
 #include "base.h"
 
+#include "hardware/timer.h"
+
 QOS_BEGIN_EXTERN_C
 
 void qos_sleep(qos_time_t timeout);
 qos_time_t qos_time();
 void qos_normalize_time(qos_time_t* time);
+
+static inline qos_time_t qos_from_absolute_time(absolute_time_t absolute_time) {
+  return to_us_since_boot(absolute_time) + INT64_MIN;
+}
 
 QOS_END_EXTERN_C
 
